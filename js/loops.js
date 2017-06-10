8cfe573d796a4f1cfa445e71f594f4b873c40c89
Michał Kozłowski <kozlowskimichal88@gmail.com>
2017-06-10 13:45:52 +0200

    pętla do while i przerywanie poprzez break

--- js/loops.js
+++ js/loops.js
@@ -19,3 +19,24 @@ while (i < 10) {
 console.log ("Pętla while [i= "+ i +"]");
 i++;
 }
+
+
+/*Pętla do...while*/
+
+var iter =20;
+do {
+    console.log (iter);
+    iter++;
+} while (iter < 10)
+    
+    
+/*Przerywanie pętli- break*/
+    
+var a = 0;
+while (a <10) {
+    console.log(++a);
+ if (a ==3) {
+     break;
+ }
+    
+}
