ebbe0517f63023af46d03de99d935b568ec9196c
Michał Kozłowski <kozlowskimichal88@gmail.com>
2017-06-10 13:26:21 +0200

    pętla while

--- js/loops.js
+++ js/loops.js
@@ -1,10 +1,21 @@
 'use strict';
 
-for ( var i =0; i<4; i++){
+/*Pętla for*/
+    
+    for ( var i =0; i<4; i++){
     console.log(i)
 }
 
 
 for ( var i =4; i>0; i--){
     console.log(i)
+}
+
+
+/*Pętla while*/
+
+var i=0;
+while (i < 10) {
+console.log ("Pętla while [i= "+ i +"]");
+i++;
 }
